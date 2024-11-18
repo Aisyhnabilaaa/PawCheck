d4334905fdee9bb4c4f9dc6eae35d5e79409fab3
Dika <dika.zulhidayat96@gmail.com>
2024-11-18 11:23:07 +0700

    fix server

--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
 const Hapi = require("@hapi/hapi");
-const authRoutes = require("./routes/authRoutes");
 
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
     host: "localhost",
+    routes: {
+      cors: true, // Untuk mengizinkan permintaan dari client yang berbeda domain
+      payload: {
+        parse: true, // Memastikan payload diproses
+        allow: "application/json", // Mengizinkan format JSON
+      },
+    },
   });
 
-  server.route(authRoutes);
+  // Tambahkan route di sini
+  server.route(require("./api/users/routes"));
 
   await server.start();
-  console.log("Server running on %s", server.info.uri);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
-});
 
 init();
